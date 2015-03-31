7126fb8cb05206210ef884593d45450ffc9c5fdf
Sven Dubbeld <sven.dubbeld1@gmail.com>
2015-03-31 14:26:30 +0200

    Updated Electives

--- pages/electives.php
+++ pages/electives.php
@@ -4,7 +4,12 @@
     certain restrictions to
     keep the electives coherent and provide the student with valuable additional information. To do this, there are
     elective-packages that make it easy for the student to choose a consistent set of electives. It is up to you to
-    decide how to fill this part of your study program.
+    decide how to fill this part of your study program:
+</p>
+<p>
+    Within your own discipline, exploring your <a href="?id=6">major</a> in greater depth.<br />
+    Within a different discipline, expanding your horizons by combining different fields.<br />
+    Opting for a combination within one of the TU/e research areas: Energy, Health and Smart Mobility.
 </p>
 <p>
     Check all available electives <a href="http://educationguide.tue.nl/bachelor-college/electives/" target="_blank">here</a>.
