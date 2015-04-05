3cedcb8c94c1d35d31aa9eabb768356be6110f4d
Sven Dubbeld <sven.dubbeld1@gmail.com>
2015-04-05 12:48:50 +0200

    Added flowchart to sitemap

--- pages/sitemap.php
+++ pages/sitemap.php
@@ -1,4 +1,4 @@
 <h1>Sitemap</h1>
 <?php include 'menu.php'; ?>
 <br />
-<img src="images/flowchart.png" alt="Flowchart" style="width: 750px;" />
+<a href="images/flowchart.png" target="_blank"><img src="images/flowchart.png" alt="Flowchart" style="width: 1000px;" /></a>
