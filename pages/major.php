c37805290c5e4a07a43d9fb0fb2aab45e651b43c
Sven Dubbeld <sven.dubbeld1@gmail.com>
2015-04-05 14:11:13 +0200

    Added major page

---
+++ pages/major.php
@@ -0,0 +1,13 @@
+<h1>Major</h1>
+<p>
+    The major is the main set of courses that define the bachelor program that is chosen. These courses make sure that
+    students following Data Science, for example, will at least master the skills required to earn a degree in Data
+    Science.
+</p>
+
+<h2>Switching Majors</h2>
+<p>
+    It is possible to switch majors, but it has to be kept in mind that many chosen and/or done <a href="?id=4">electives</a> might not be
+    eligible anymore after the change: this might be because the electives suddenly overlap with major courses. In any
+    chance, changing your major will almost always lead to additional study time.
+</p>
